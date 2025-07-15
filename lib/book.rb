aeff4ff581b4fefba75a403d291dcf7598939ec3
Mike <mclancy96@gmail.com>
2025-07-15 12:40:30 -0400

    Create book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_reader :title
+  attr_accessor :genre, :page_count, :author
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
